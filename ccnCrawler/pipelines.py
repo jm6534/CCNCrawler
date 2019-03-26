dd297591356445f2758d6d79d60c9527425d3be3
Yeongbin kang <clear1014@gmail.com>
2019-03-26 14:14:22 +0900

    crawling, file updating

--- ccnCrawler/pipelines.py
+++ ccnCrawler/pipelines.py
@@ -4,8 +4,51 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from __future__ import unicode_literals
+from scrapy.exporters import JsonItemExporter, CsvItemExporter
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
 
+import json
 
-class CcncrawlerPipeline(object):
+#
+#class CcncrawlerPipeline(object):
+#    def process_item(self, item, spider):
+#        return item
+
+class JsonPipeline(object):
+    
+    def __init__(self):
+        #move (old)new.json to history
+        self.historyFile = open("history.json", 'rt', encoding='UTF-8')
+        self.newFile = open("new.json","rt", encoding='UTF-8')
+        try:
+            self.old = json.loads(self.historyFile.read())
+        except:
+            self.old = json.loads("[]")
+        try:
+            self.new = json.loads(self.newFile.read())
+        except:
+            self.new  =json.loads("[]")
+        self.historyFile.close()
+        self.newFile.close()
+        
+        with open("history.json","wt",encoding='UTF-8') as outfile:
+            json.dump(self.old+self.new, outfile, indent=4,ensure_ascii = False)
+        
+        self.historyFile = open("history.json", 'rt', encoding='UTF-8')
+        self.articles = json.loads(self.historyFile.read())
+        self.newFile = open("new.json", 'wb')
+        self.exporter = JsonItemExporter(self.newFile, encoding='utf-8', ensure_ascii=False, indent = 4)
+        self.exporter.start_exporting()
+ 
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.newFile.close()
+ 
     def process_item(self, item, spider):
-        return item
+        for article in self.articles:
+            if (item["title"] == article["title"]):
+                return
+        self.exporter.export_item(item)
---
+++ ccnCrawler/spiders/sw_spider.py
@@ -0,0 +1,35 @@
+import scrapy
+import time
+from ccnCrawler.items import CcncrawlerItem
+import logging
+
+
+class SwSpider(scrapy.Spider):
+    name = "sw"
+
+    def start_requests(self):
+        urls = [
+            "https://sw.cau.ac.kr/board/list?boardtypeid=7&menuid=001005005",
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        url =  "https://sw.cau.ac.kr/board"
+
+        for i in range(1,11):
+            item = CcncrawlerItem()
+            logging.error(i)
+            logging.error(response.xpath('//*[@id="boardForm"]/div[2]/ul/li['+ str(i) + ']/div/p[1]/a/text()').extract())
+
+            item['url'] = url+response.xpath('//*[@id="boardForm"]/div[2]/ul/li['+ str(i) +']/div/p[1]/a/@href').extract()[0][1:]
+            try:
+                item['title'] = response.xpath('//*[@id="boardForm"]/div[2]/ul/li['+ str(i) + ']/div/p[1]/a/text()').extract()[1]
+            except:
+                item['title'] = response.xpath('//*[@id="boardForm"]/div[2]/ul/li['+ str(i) + ']/div/p[1]/a/text()').extract()[0].strip()
+            
+            yield item
+
+        
+        
+        
